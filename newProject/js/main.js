aedd9bdf0aa8650245e66fc0414334d6e751084f
Nurpakus <42069875+Nurpakus@users.noreply.github.com>
2023-02-21 20:04:24 +0700

    Изменения
    Сайт создан

---
+++ newProject/js/main.js
@@ -0,0 +1,17 @@
+
+window.onload=function(){
+    document.getElementById('knopkafeedback').addEventListener('click', function () {
+        document.getElementById('forma').style.display = 'block';
+    })
+
+    document.getElementById('knopkatelegram').addEventListener('click', function () {
+        document.getElementById('telegram').style.display = 'block';
+    })
+    document.getElementById('knopkawhatsApp').addEventListener('click', function () {
+        document.getElementById('whatsApp').style.display = 'block';
+    })
+    document.getElementById('knopkagmail').addEventListener('click', function () {
+        document.getElementById('gmail').style.display = 'block';
+    })
+    
+}
